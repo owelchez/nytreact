2a86388e3ad61cad82e07e7d39b95b61f7deecfd
Orvin Welchez <owelchez@gmail.com>
2016-09-20 01:02:01 -0400

    db

---
+++ server.js
@@ -0,0 +1,60 @@
+// Include Server Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+//Require Click Schema
+var Article = require('./models/Article.js');
+
+// Create Instance of Express
+var app = express();
+var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
+
+// Run Morgan for Logging
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.use(express.static('./public'));
+
+// -------------------------------------------------
+
+mongoose.connect('mongodb://localhost/nytimes');
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+  console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function () {
+  console.log('Mongoose connection successful.');
+});
+
+
+// -------------------------------------------------
+
+app.get('/', function(req, res){
+  res.sendFile('./public/index.html');
+});
+
+
+app.get('/api/', function(req, res) {
+  db.find()(function(err, doc){
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.send(doc);
+    }
+  })
+});
+
+// -------------------------------------------------
+
+// Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
